e9af154b7b8a3db904c8f0f4973f5ef13f894ce9
Your Name <sharunnd11@gmail.com>
2024-04-28 19:42:36 +0530

    FE and BE setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,35 @@
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+import Layout from "./layouts/Layout";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <p>Home Page</p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <p>Search Page</p>
+            </Layout>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
