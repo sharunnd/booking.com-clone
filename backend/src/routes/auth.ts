a784e9dc0eb98273c7c1fefb5aa805c2fe76fa72
Your Name <sharunnd11@gmail.com>
2024-04-30 23:23:05 +0530

    token validation

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -4,6 +4,7 @@ import { check, validationResult } from "express-validator"
 import User from "../models/user"
 import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
+import verfyToken from "../middleware/auth"
 
 const router = express.Router()
 
@@ -46,4 +47,8 @@ router.post("/login",[
        }
    
 )
+
+router.get("/validate-token",verfyToken,(req:Request,res:Response)=>{
+    res.status(200).send({userId:req.userId})
+})
 export default router
