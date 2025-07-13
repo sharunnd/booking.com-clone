ae4d651c56365fda0940cef12b199ac5b7782dc9
Your Name <sharunnd11@gmail.com>
2025-07-14 00:09:15 +0530

    add fetch api hotels for home page

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -53,6 +53,16 @@ router.get("/search", async (req: Request, res: Response) => {
   }
 });
 
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const hotels = await Hotel.find().sort("-lastUpdated");
+    res.json(hotels);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({ message: "Error fetching hotels" });
+  }
+});
+
 router.post(
   "/:hotelId/bookings/payment-intent",
   verifyToken,
